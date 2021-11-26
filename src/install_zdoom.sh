4f4389f90cc5ba7343406df0c06dbd93d41b920d
Steffen Killinger <info@killinger-it.com>
2021-11-26 18:14:02 +0100

    added correct path

--- src/install_zdoom.sh
+++ src/install_zdoom.sh
@@ -7,21 +7,21 @@ sudo cp /opt/retropie/ports/zdoom/zdoom.pk3 /opt/retropie/ports/zdoom/zdoom.pk3_
 
 #overwrite my compiled files for the Retrostone 2
 echo "overwrite compiled files for retrostone 2"
-sudo cp src/functional_zdoom/zdoom /opt/retropie/ports/zdoom/zdoom
-sudo cp src/functional_zdoom/zdoom.pk3 /opt/retropie/ports/zdoom/zdoom.pk3
+sudo cp functional_zdoom/zdoom /opt/retropie/ports/zdoom/zdoom
+sudo cp functional_zdoom/zdoom.pk3 /opt/retropie/ports/zdoom/zdoom.pk3
 sudo chown pi:pi /opt/retropie/ports/zdoom/zdoom
 sudo chown pi:pi /opt/retropie/ports/zdoom/zdoom.pk3
 sudo chmod 0755 /opt/retropie/ports/zdoom/zdoom
 sudo chmod 0644 /opt/retropie/ports/zdoom/zdoom.pk3
 
 echo "copy start files for Doom1 to emulator"
-sudo cp src/functional_zdoom/Doom_1.sh /home/pi/RetroPie/roms/ports/Doom_1.sh
-sudo cp src/functional_zdoom/Doom_1\ Controller.sh /home/pi/RetroPie/roms/ports/Doom_1\ Controller.sh
+sudo cp functional_zdoom/Doom_1.sh /home/pi/RetroPie/roms/ports/Doom_1.sh
+sudo cp functional_zdoom/Doom_1\ Controller.sh /home/pi/RetroPie/roms/ports/Doom_1\ Controller.sh
 sudo chown pi:pi /home/pi/RetroPie/roms/ports/Doom_1.sh
 sudo chown pi:pi /home/pi/RetroPie/roms/ports/Doom_1\ Controller.sh
 sudo chmod 0755 /home/pi/RetroPie/roms/ports/Doom_1.sh
 sudo chmod 0755 /home/pi/RetroPie/roms/ports/Doom_1\ Controller.sh
-cp src/functional_zdoom/zdoom.ini_doom_retrostone /home/pi/.config/zdoom/zdoom.ini_doom_retrostone
-cp src/functional_zdoom/zdoom.ini_doom_controller /home/pi/.config/zdoom/zdoom.ini_doom_controller
+cp functional_zdoom/zdoom.ini_doom_retrostone /home/pi/.config/zdoom/zdoom.ini_doom_retrostone
+cp functional_zdoom/zdoom.ini_doom_controller /home/pi/.config/zdoom/zdoom.ini_doom_controller
 
 echo "functional zdoom installed, please restart emulationstation"
